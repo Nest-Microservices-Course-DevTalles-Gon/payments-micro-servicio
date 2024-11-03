a0ce404c4c56c1bcdbbf1d5ab9323b5edff4ac4d
Gonzalo Buasso <gonzalobuasso@gmail.com>
2024-11-03 10:47:00 +0100

    Updated logger main

--- src/main.ts
+++ src/main.ts
@@ -32,6 +32,6 @@ async function bootstrap() {
 
   await app.listen(envs.port);
 
-  logger.log(`Payments Microservice running on port ${envs.port}`);
+  logger.log(`Payments-MS running on port ${envs.port}`);
 }
 bootstrap();
